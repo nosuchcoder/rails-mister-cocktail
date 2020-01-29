21fa3380f8a9e7918246f2456b8ad452a5dc05d4
Kwame Dalrymple <neokwame@tutanota.com>
2020-01-29 21:25:20 +0000

    changed routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'cocktails#index'
+  root to: 'cocktails#index'
   resources :cocktails do
     resources :doses, only: [:index, :edit, :create, :show, :new, :destroy]
   end
